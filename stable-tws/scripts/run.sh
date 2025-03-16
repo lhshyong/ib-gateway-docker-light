b06683d47aac98f48c9110a0be5aa58fe4900e80
shyong <user@picosoft>
2025-03-16 16:03:15 +0800

    Add stable tws

--- stable-tws/scripts/run.sh
+++ stable-tws/scripts/run.sh
@@ -4,7 +4,7 @@ set -x
 export DISPLAY=:1
 
 rm -f /tmp/.X1-lock
-Xvfb :1 -ac -screen 0 1024x768x16 &
+Xvfb :1 -ac -screen 0 1280x800x16 &
 
 if [ -n "$VNC_SERVER_PASSWORD" ]; then
   echo "Starting VNC server"
@@ -15,7 +15,7 @@ envsubst < "${IBC_INI}.tmpl" > "${IBC_INI}"
 
 /root/scripts/fork_ports_delayed.sh &
 
-/root/ibc/scripts/ibcstart.sh "${TWS_MAJOR_VRSN}" -g \
+/root/ibc/scripts/ibcstart.sh "${TWS_MAJOR_VRSN}" \
      "--tws-path=${TWS_PATH}" \
      "--ibc-path=${IBC_PATH}" "--ibc-ini=${IBC_INI}" \
      "--user=${TWS_USERID}" "--pw=${TWS_PASSWORD}" "--mode=${TRADING_MODE}" \
